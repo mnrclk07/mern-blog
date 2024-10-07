05990329bcfce37c669fbd90e2aca45886ea3854
mnrclk07 <mnrclk21@gmail.com>
2024-10-07 22:12:09 +0300

    complete sidebar of the dashboard

--- client/src/pages/Dashboard.jsx
+++ client/src/pages/Dashboard.jsx
@@ -1,7 +1,27 @@
-import React from "react";
+import { useEffect, useState } from "react";
+import DashSidebar from "../components/DashSidebar";
+import DashProfile from "../components/DashProfile";
+import { useLocation } from "react-router-dom";
 
-function Dashboard() {
-  return <div>Dashboard</div>;
+export default function Dashboard() {
+  const [tab, setTab] = useState("");
+  const location = useLocation();
+
+  useEffect(() => {
+    const urlParams = new URLSearchParams(location.search);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
+    }
+  }, [location.href]);
+  return (
+    <div className="min-h-screen flex flex-col md:flex-row">
+      <div className="md:w-56">
+        {/* Sidebar */}
+        <DashSidebar />
+      </div>
+      {/* profile... */}
+      {tab === "profile" && <DashProfile />}
+    </div>
+  );
 }
-
-export default Dashboard;
